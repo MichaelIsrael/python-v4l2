416537f7a02e2aab741961dde9bcaa34369faa74
Tamas Kemenczy <tamas.kemenczy@gmail.com>
2009-08-14 00:29:29 -0400

    Fixed typos in setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ setup(
     name='v4l2',
     version='0.1',
     license='GPLv2',
-    requires=('ctypes',)
-    py_modules=('v4l2',)
+    requires=('ctypes',),
+    py_modules=('v4l2',),
 
     maintainer='tamask',
     maintainer_email='tamas.kemenczy@gmail.com',
     url='http://pypi.python.org/pypi/v4l2',
-    keywords'v4l2 video4linux video4linux2 binding ctypes',
+    keywords='v4l2 video4linux video4linux2 binding ctypes',
     description='Python bindings for the v4l2 userspace api.',
 
     classifiers=(
